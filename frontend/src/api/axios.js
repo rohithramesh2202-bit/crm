fbf5fdbfaf25f7f05fadebd307f3bd75be83ce96
RohithMR <rohith.ramesh2202@gmail.com>
2026-08-11 15:02:39 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const baseURL = "https://crm-ce6l.vercel.app";
 
 const api = axios.create({
   baseURL,
